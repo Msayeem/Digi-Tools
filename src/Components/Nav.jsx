c290a27bd98587c8e9f1547e4e7d92c1c4fc4b3b
Msayeem <msayeem223@gmail.com>
2026-03-29 22:00:04 +0600

    first commit

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import i1 from '../../assets/products/design-tool.png'
+
+const Nav = () => {
+    return (
+       <nav>
+        <div><img src={i1} alt="" /></div>
+       </nav>
+    );
+};
+
+export default Nav;
