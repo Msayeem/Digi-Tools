22304143499a1882cfa3ca85965cc226c39c4dbf
Msayeem <msayeem223@gmail.com>
2026-03-29 22:30:28 +0600

    Navbar added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Nav from './Components/Nav'
 
 function App() {
 
 
   return (
     <>
-      
+      <header>
+        <Nav></Nav>
+      </header>
     </>
   )
 }
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import i1 from '../../assets/products/design-tool.png'
+import { FiShoppingCart } from 'react-icons/fi';
 
 const Nav = () => {
     return (
-       <nav>
-        <div><img src={i1} alt="" /></div>
+       <nav className='py-4 lg:w-[90%] w-[95%] mx-auto flex justify-between items-center'>
+        <div className='text-3xl font-bold'><h1 className='bg-gradient-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent'>DigiTools</h1></div>
+       
+       <ul className='flex items-center gap-5'>
+        <li><a className='font-semibold text-[16px]' href="">Products</a></li>
+        <li><a className='font-semibold text-[16px]' href="">Features</a></li>
+        <li><a className='font-semibold text-[16px]' href="">Pricing</a></li>
+        <li><a className='font-semibold text-[16px]' href="">Testimonials</a></li>
+        <li><a className='font-semibold text-[16px]' href="">FAQ</a></li>
+       </ul>
+
+       <div className='flex items-center gap-3'>
+        <span><FiShoppingCart></FiShoppingCart></span>
+        <button>Login</button>
+        <button className='bg-gradient-to-r from-[#4F39F6] to-[#9514FA] text-white text-[16px] font-semibold px-[16px] py-[12px] rounded-3xl'>Get Started</button>
+       </div>
        </nav>
     );
 };
